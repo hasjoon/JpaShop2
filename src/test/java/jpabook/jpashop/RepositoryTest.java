ec732eda7c51c24791452541df485d9ea1cfdc5e
hasjoon <osn6629@naver.com>
2022-05-13 23:19:55 +0900

    JpaShop 시작

---
+++ src/test/java/jpabook/jpashop/RepositoryTest.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import static org.junit.Assert.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUserName("memberA");
+
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+
+    }
+
+}
