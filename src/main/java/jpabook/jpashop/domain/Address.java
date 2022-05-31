666aa8b4a172d3050e1b066300aae88edb4b8fbb
hasjoon <osn6629@naver.com>
2022-05-31 12:52:39 +0900

    restartedMain 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -7,7 +7,19 @@ import lombok.Getter;
 @Getter
 public class Address {
 
+
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
 }
