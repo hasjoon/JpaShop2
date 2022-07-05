c0c83c91d60ade724790d1ec0bf01bb0abcc5b1c
hasjoon <osn6629@naver.com>
2022-07-06 00:20:51 +0900

    memberService tdd 추가

--- src/main/java/jpabook/jpashop/domain/Service/MemberService.java
+++ src/main/java/jpabook/jpashop/domain/Service/MemberService.java
@@ -3,39 +3,45 @@ package jpabook.jpashop.domain.Service;
 import java.util.List;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
+@Transactional(readOnly = true) // 읽기에는 가급적 이걸 넣어줌 (데이텨 변경 필오 없음)
+@RequiredArgsConstructor //final이 있는 필드만 가지고 생성자 만들어줌
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+
+    private final MemberRepository memberRepository;
+
 
     /**
      * 회원가입
      */
+    @Transactional //기본은 false임
     public Long join(Member member){
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
-        //em.persist할때 이미 키값이 고정이됨
+        //em.persist할때 이미 키값이 고정이됨 -> member엔티티에서 generatedvalue 해줬기때문
     }
 
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
         if(!findMembers.isEmpty()){
-            throw new IllegalStateException("이미 존재하는 회원입니다.");//5:24 최적화방법
+            throw new IllegalStateException("이미 존재하는 회원입니다.");//최적화 방법 = 사이즈를 재서 findmembers 가 0이상이면 익셉션터트림
         }
-
     }
 
 
     //회원 전체 조회
-    @GetMapping("/v1/member")
-    public void findAllMember(){
-        List<Member> allMember = memberRepository.findAll();
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
     }
-
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
 }
---
+++ src/test/java/jpabook/jpashop/domain/Service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.domain.Service;
+
+//import static org.junit.Assert.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    @DisplayName("회원가입 정상작동 확인 TEST")
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("Kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        System.out.println("결과값");
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        //given
+
+        //when
+
+        //then
+    }
+
+}
