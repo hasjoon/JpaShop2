47a8ee8518779b36d39ecafd760a6f934b680250
hasjoon <osn6629@naver.com>
2022-07-06 12:40:59 +0900

    회원가입 TDD 추가

--- src/test/java/jpabook/jpashop/domain/Service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/domain/Service/MemberServiceTest.java
@@ -4,6 +4,7 @@ package jpabook.jpashop.domain.Service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javax.persistence.EntityManager;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,10 +26,13 @@ public class MemberServiceTest {
     MemberService memberService;
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
 
 
     @Test
     @DisplayName("회원가입 정상작동 확인 TEST")
+    @Rollback(false)
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
@@ -37,7 +42,7 @@ public class MemberServiceTest {
         Long savedId = memberService.join(member);
 
         //then
-        System.out.println("결과값");
+        em.flush();
         assertEquals(member, memberRepository.findOne(savedId));
     }
 
