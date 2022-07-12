962632ba199d1d122d394028342d6c67cb2e2721
hasjoon <osn6629@naver.com>
2022-07-13 00:44:09 +0900

    주문생성메서드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,6 +18,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 @Entity
 @Table(name = "orders")
@@ -45,4 +46,59 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 (ORDER, CANCEL)
+
+    //==연관관계 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void serDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order creatOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문취소
+     * */
+    public void cancel(){
+        if (delivery.getStatus() == DeliveryStatus.CAMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다. ");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /***
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+
+        //스트림이나 람다를 활용하면 깔끔하게 작업할 수 있음
+        //ctrl + alt + n -> 코드 줄여줌 인라인화 라고함
+    }
 }
