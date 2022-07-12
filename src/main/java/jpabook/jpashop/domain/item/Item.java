39f859fe9483a4c6114334a5696143ea9cd5a2c3
hasjoon <osn6629@naver.com>
2022-07-12 23:44:18 +0900

    Item 수량확인 테스트 코드 작성

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -45,7 +45,7 @@ public class Item {
      * stock 감소
      * */
     public void removeStock(int quantity) {
-        int restStock = this.stockQuantity -= quantity;
+        int restStock = this.stockQuantity - quantity;
         if (restStock < 0){
             throw new NotEnoughStockException("need more stock");
         }
