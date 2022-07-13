b45f4c8306cbb1003d9fc70d53165b25d91425f4
hasjoon <osn6629@naver.com>
2022-07-13 12:44:44 +0900

    OrderService TEST 업데이트

--- src/test/java/jpabook/jpashop/domain/Service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/domain/Service/OrderServiceTest.java
@@ -14,7 +14,6 @@ import jpabook.jpashop.domain.repository.ItemRepository;
 import jpabook.jpashop.domain.repository.MemberRepository;
 import jpabook.jpashop.domain.repository.OrderRepository;
 import jpabook.jpashop.exception.NotEnoughStockException;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
@@ -26,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
+//테스트당 끝나면 롤백을 해준다
 public class OrderServiceTest {
 
     @Autowired
@@ -40,20 +40,12 @@ public class OrderServiceTest {
     EntityManager em;
 
 
-
     @Test
-    public void 상품주문() throws Exception{
-      //given
-        Member member = new Member();
-        member.setName("Kim1");
-        member.setAddress(new Address("서울", "도봉구", "1111"));
-        em.persist(member);
+    public void 상품주문() throws Exception {
+        //given
+        Member member = creatMember("Kim1", new Address("서울", "도봉구", "1111"));
 
-        Book book = new Book();
-        book.setName("서울 JPA");
-        book.setPrice(10000);
-        book.setStockQuantity(10);
-        em.persist(book);
+        Book book = creatBook("서울 JPA", 10000, 10);
 
         int orderCount = 2;
 
@@ -65,31 +57,77 @@ public class OrderServiceTest {
 
         assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
         assertEquals("주문한 상품 종류 수가 정확해야 한다", 1, getOrder.getOrderItems().size());
-        assertEquals("주문 가격 = 가격 * 수량 이다.", 10000*orderCount, getOrder.getTotalPrice());
-        assertEquals("주문 수량만큼 재고가 줄어야한다.", 8 ,book.getStockQuantity());
+        assertEquals("주문 가격 = 가격 * 수량 이다.", 10000 * orderCount, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야한다.", 8, book.getStockQuantity());
 
     }
 
     @Test
-    public void 주문취소() throws Exception{
-      //given
+    public void 주문취소() throws Exception {
+        //given
+        Member member = creatMember("Kim1", new Address("서울", "도봉구", "1111"));
+        Book book = creatBook("서울 JPA", 10000, 10);
 
-      //when
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
 
-      //then
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order order = orderRepository.findOne(orderId);
+
+        assertEquals("주문 취소시 상태는 CANCLE 이다.", OrderStatus.CANCEL, order.getStatus());
+        assertEquals("상품 수량 복구 확인", 10, book.getStockQuantity());
 
     }
 
     @Test(expected = NotEnoughStockException.class)
     @DisplayName("주문수량초과 TEST")
-    public void 주문수량초과() throws Exception{
+    public void 주문수량초과() throws Exception {
         //given
         Item item = new Item();
         item.setStockQuantity(3);
         //when
-        OrderItem.creatOrderItem(item, 1000, 4 );
+        OrderItem.creatOrderItem(item, 1000, 4);
 
         //then
         fail("need more stock");
+    }
+
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+        Member member = creatMember("kim2", new Address("인천", "계양구", "123-123"));
+        Item item = creatBook("시골 JPA", 10000, 10);
+
+        int orderCount = 11;
+        //when
+        orderService.order(member.getId(),item.getId(),orderCount);
+
+
+        //then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+
+
+
+
+    private Book creatBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member creatMember(String name, Address address) {
+        Member member = new Member();
+        member.setName(name);
+        member.setAddress(address);
+        em.persist(member);
+        return member;
     }
 }
