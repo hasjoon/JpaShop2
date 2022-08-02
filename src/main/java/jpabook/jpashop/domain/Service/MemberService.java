6218c4204179652fb2599762c9662a93bbc703ef
hasjoon <osn6629@naver.com>
2022-08-02 23:41:08 +0900

    회원가입 기능 추가

--- src/main/java/jpabook/jpashop/domain/Service/MemberService.java
+++ src/main/java/jpabook/jpashop/domain/Service/MemberService.java
@@ -10,8 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
-@Transactional(readOnly = true) // 읽기에는 가급적 이걸 넣어줌 (데이텨 변경 필오 없음)
-@RequiredArgsConstructor //final이 있는 필드만 가지고 생성자 만들어줌
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
 
@@ -21,12 +21,11 @@ public class MemberService {
     /**
      * 회원가입
      */
-    @Transactional //기본은 false임
+    @Transactional
     public Long join(Member member){
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
-        //em.persist할때 이미 키값이 고정이됨 -> member엔티티에서 generatedvalue 해줬기때문
     }
 
     private void validateDuplicateMember(Member member) {
@@ -35,7 +34,6 @@ public class MemberService {
             throw new IllegalStateException("이미 존재하는 회원입니다.");//최적화 방법 = 사이즈를 재서 findmembers 가 0이상이면 익셉션터트림
         }
     }
-
 
     //회원 전체 조회
     public List<Member> findMembers(){
